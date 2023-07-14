2aa3036ab498e58cdd9d5cbc193b49c4d47e1651
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-07-14 23:59:53 +0500

    yangi elementlar qo'shildi

--- phyton-lug'at.py
+++ phyton-lug'at.py
@@ -1,3 +1,19 @@
+#1.bu yerda kalit so'z va qiymat ishtirok qiladi model,rang--> kalit so'z ferrari, ko'k --> qiymat
+
 car_0={"model":"Ferrari","rang":"ko'k"}
 print(car_0["model"])
 print(car_0["rang"])
+
+#2.
+
+eng_uz={"apple":"olma","banana":"banan","watermelon":"tarvuz"}
+print(eng_uz["apple"])
+print(eng_uz['banana'])
+print(eng_uz['watermelon'])
+
+#3.
+talaba={"ism":"Karimjon","yosh":20,"yil":2003}
+#print(f"Talabani ismi {talaba['ism']} yoshi {talaba['yosh']} da {talaba['yil']}da tug'ilgan")
+talaba["kurs"]=3
+talaba["fakultet"]="Amaliy matematika"
+print(talaba)
