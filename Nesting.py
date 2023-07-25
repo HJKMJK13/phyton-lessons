6ee01b215d69767e59eea28fd0947d0dccf428ab
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-07-25 17:07:21 +0500

    turli misollar yordamida tushunib olamiz

--- Nesting.py
+++ Nesting.py
@@ -17,5 +17,37 @@ car_3={
        }
 cars=[car_1,car_2,car_3]
 for car in cars:
-    print(f"{car['model'].title()}, {car['narx']}$ , Maksimal tezligi:{car['max_tezlik']}")
+    print(f"{car['model'].title()}, narxi:{car['narx']}$ , Maksimal tezligi:{car['max_tezlik']}")
 
+#
+print(cars[0]['model'],cars[0]['narx'],cars[0]['max_tezlik'])
+print(cars[1]['model'],cars[1]['narx'],cars[1]['max_tezlik'])
+print(cars[2]['model'],cars[2]['narx'],cars[2]['max_tezlik'])
+
+#
+
+lacettis=[]
+for n in range(5):
+    new_car={
+        'model':'lacetti',
+        'rang':None,
+        'probeg':0,
+        'Narx':None,
+        'tezlik':200
+        }
+    lacettis.append(new_car)
+# =============================================================================
+# for lacetti in lacettis:
+#     print(lacetti)
+# =============================================================================
+for lacetti in lacettis[:3]:
+    lacetti['rang']='qora'
+    
+for lacetti in lacettis[3:5]:
+    lacetti['rang']='kok'
+
+for lacetti in lacettis[:5]:
+    lacetti['Narx']=22000
+    
+for lacetti in lacettis:
+    print(lacetti)
