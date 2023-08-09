f213a29fc760ade2bce0640fd5ff5b9529c8e889
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-08-09 17:37:00 +0500

    while operatori ishora yordamida sikilni to'xtatish

--- while.py
+++ while.py
@@ -1,1 +1,19 @@
+#1.
+son=0
+while son<=100:
+    son=son+1
+    print(son)
+print("sikl to'xtadi")
+
+#2.input va while
+savol="Istalgan sonni kiriting"
+savol +="(dasturni to'xtatish un 'exit' deb yozing):"
+qiymat=''
+while qiymat !='exit':
+    qiymat=input(savol)
+    if qiymat !='exit':
+        print(float(qiymat)**2)
+print("Dastur tugadi!")
+
+
 
