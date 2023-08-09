8858d786b7d2c68f6db8b6c0c62cc9cff8689611
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-08-09 18:28:27 +0500

    for sikliga doir misollar bajarildi

--- while.py
+++ while.py
@@ -1,58 +1,51 @@
 #1.
-# =============================================================================
-# son=0
-# while son<=100:
-#     son=son+1
-#     print(son)
-# print("sikl to'xtadi")
-# =============================================================================
+son=0
+while son<=100:
+    son=son+1
+    print(son)
+print("sikl to'xtadi")
 
-# =============================================================================
-# #2.input va while
-# savol="Istalgan sonni kiriting"
-# savol +="(dasturni to'xtatish un 'exit' deb yozing):"
-# qiymat=''
-# while qiymat !='exit':
-#     qiymat=input(savol)
-#     if qiymat !='exit':
-#         print(float(qiymat)**2)
-# print("Dastur tugadi!")
-# =============================================================================
+#2.input va while
+savol="Istalgan sonni kiriting"
+savol +="(dasturni to'xtatish un 'exit' deb yozing):"
+qiymat=''
+while qiymat !='exit':
+    qiymat=input(savol)
+    if qiymat !='exit':
+       print(float(qiymat)**2)
+print("Dastur tugadi!")
 
-#3.Ishora orqali siklni to'xtatish
-# =============================================================================
-# savol=input("Istalgan sonni kiriting(dasturni to\'xtatish un 'exit' deb yozing):")
-# savol=float(savol)
-# ishora=True
-# while ishora:
-#       qiymat=input(savol)
-#       if qiymat=='exit':
-#          ishora=False
-#       else:
-#          print(float(qiymat)**2)
-#       
-# =============================================================================
-#4.break operatori
-# =============================================================================
-# savol=input("Istalgan sonni kiriting(dasturni to\'xtatish un 'exit' deb yozing):")
-# while True:
-#     qiymat=input(savol)
-#     if qiymat=='exit':
-#         break
-#     else:
-#         print(float(qiymat)**2)
-# =============================================================================
-
-#5.
+#3.
 sonlar=list(range(1,11))
 for son in sonlar:
     if son==5:
         break
     print(f"{son}ning kvadrati {son**2} ga teng")
 
-#6.continue
+#4.continue--> tashlab ketish vazifasini bajaradi
 sonlar=list(range(1,10))
 for son in sonlar:
     if son==5:
         continue
     print(f"{son} ning kvadrati {son**2} ga teng")
+
+#5.
+son=0
+while son<10:
+    son+=1
+    if son%2==0:
+        continue
+    else:
+        print(son)
+#6.
+sonlar=list(range(1,100))
+for son in sonlar:
+    if son%2==0:
+        continue
+    print(son)   #--> toq sonlar chiqadi
+
+#7.
+sonlar=list(range(1,100))
+for son in sonlar:
+    if son%2==0:
+       print(son)
