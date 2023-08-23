43aef2eaea90306ae39a15c3e0ecd04e3eb47902
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-08-23 13:49:20 +0500

    docstringni konsolga chiqarishni o'rgandik

--- funksiya.py
+++ funksiya.py
@@ -7,5 +7,12 @@ salom_ber()
 # 2.
 def salom_ber(ism):
     """Foydalanuvchining ismini qabul qilib unga salom beruvchi funksiya"""
-    print(f"Assalomu aleykum hurmatli {ism}")
+    print(f"Assalomu aleykum hurmatli {ism.title()}")
 salom_ber('Karimjon')
+
+# 3.docstring
+def salom_ber(ism):
+    """Foydalanuvchining ismini qabul qilib unga salom beruvchi funksiya"""
+    print(f"Assalomu aleykum hurmatli {ism.title()}")
+salom_ber('Karimjon')
+print(salom_ber.__doc__)
