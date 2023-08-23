bdd0297c3d6d66501315d01304e835425641155b
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-08-23 13:45:18 +0500

    funksiyaga doir eng oddiy misollar

--- funksiya.py
+++ funksiya.py
@@ -1,4 +1,11 @@
+# 1.
 def salom_ber():
     """Salom beruvchi funksiya"""
     print("Assalomu aleykum")
 salom_ber()
+
+# 2.
+def salom_ber(ism):
+    """Foydalanuvchining ismini qabul qilib unga salom beruvchi funksiya"""
+    print(f"Assalomu aleykum hurmatli {ism}")
+salom_ber('Karimjon')
