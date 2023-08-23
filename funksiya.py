63887d86d19b30693204b774138dfd83b8a919b7
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-08-23 13:55:55 +0500

    funksiyaga bir nechta misol kiritishga doir misol ham kordik

--- funksiya.py
+++ funksiya.py
@@ -15,4 +15,10 @@ def salom_ber(ism):
     """Foydalanuvchining ismini qabul qilib unga salom beruvchi funksiya"""
     print(f"Assalomu aleykum hurmatli {ism.title()}")
 salom_ber('Karimjon')
-print(salom_ber.__doc__)
+print(salom_ber.__doc__) #--> docstringni konsolga chiqarish
+
+# 4.
+def salom_ber(ism,familya):
+    """Foydalanuvchining ism familyasini chiqaruvchi funksiya"""
+    print(f"Foydalanuvchining ismi:{ism.title()} \nFoydalanuvchining familyasi:{familya.title()}")
+salom_ber('Karimjon','Norboyev')
