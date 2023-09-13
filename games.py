04ee302390e8311c64c6999edb3f096b984b4072
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-09-13 21:01:39 +0500

    1-qadamda kompyuter random orqali tanlagan sonni topishga harakat qilamiz -->dasturini tuzamiz

--- games.py
+++ games.py
@@ -1,14 +1,15 @@
 import random
 def sontop(x=10):
     tasodifiy_son=random.randint(1, x)
-    print("Men bir son kirittim topa olasizmi ?")
-    
+    print("Men bir son o'yladim topa olasizmi ?")
+    taxminlar=0
     while True:
         taxmin=int(input("-->"))
+        taxminlar +=1
         if taxmin<tasodifiy_son:
             print("Xato.Men o'ylagan son kiritgan soninggizdan kattaroq")
         elif taxmin>tasodifiy_son:
             print("Xato.Men o'ylagan son kiritgan soninggizdan kichikroq")
         else:
             break
-    print("Tabriklaymiz.Topdingiz")
+    print(f"Tabriklaymiz.{taxminlar} ta taxmin bilan topdingiz")
